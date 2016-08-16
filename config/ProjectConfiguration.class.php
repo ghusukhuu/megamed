8a691f36a14dc31f7d44f80e33711ce1a1778e66
githubusukhuu <m.usukhbayar@gmail.com>
2016-08-16 17:07:57 +0800

    init megamed config

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'D://symfony//symfony-1.4.19//lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfDoctrinePlugin');
+  }
+}
