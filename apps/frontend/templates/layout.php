86537a5514da28f6711d6cd42063eed7baeafe36
githubusukhuu <m.usukhbayar@gmail.com>
2016-08-17 13:02:55 +0800

    init template

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -1,14 +1,42 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
+<!--[if !IE]><!-->
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-  <head>
-    <?php include_http_metas() ?>
-    <?php include_metas() ?>
-    <?php include_title() ?>
-    <link rel="shortcut icon" href="/favicon.ico" />
-    <?php include_stylesheets() ?>
-    <?php include_javascripts() ?>
-  </head>
-  <body>
-    <?php echo $sf_content ?>
-  </body>
+    <head>
+        <!-- Mobile Meta -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <?php include_http_metas() ?>
+        <?php include_metas() ?>
+        <?php include_title() ?>
+
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="images/favicon.ico"/>
+
+        <!-- Web Fonts -->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700,300&amp;subset=latin,latin-ext' rel='stylesheet' type='text/css'/>
+        <link href='http://fonts.googleapis.com/css?family=PT+Serif' rel='stylesheet' type='text/css'/>
+
+        <!-- Bootstrap core CSS -->
+        <link href="bootstrap/css/bootstrap.css" rel="stylesheet"/>
+
+        <!-- Font Awesome CSS -->
+        <link href="fonts/font-awesome/css/font-awesome.css" rel="stylesheet"/>
+
+        <!-- Fontello CSS -->
+        <link href="fonts/fontello/css/fontello.css" rel="stylesheet"/>
+
+        <!-- Plugins -->
+        <link href="plugins/rs-plugin/css/settings.css" media="screen" rel="stylesheet"/>
+        <link href="plugins/rs-plugin/css/extralayers.css" media="screen" rel="stylesheet"/>
+        <link href="plugins/magnific-popup/magnific-popup.css" rel="stylesheet"/>
+        <link href="css/animations.css" rel="stylesheet"/>
+        <link href="plugins/owl-carousel/owl.carousel.css" rel="stylesheet"/>
+
+        <?php include_stylesheets() ?>
+        <?php include_javascripts() ?>
+    </head>
+    <body>
+        <?php echo $sf_content ?>
+    </body>
 </html>
