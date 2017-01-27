26ac62925cfdd1910fb4311fc8eaca6aeb7e384f
githubusukhuu <m.usukhbayar@gmail.com>
2017-01-27 15:10:54 +0800

    dbg

--- web/index.php
+++ web/index.php
@@ -1,7 +1,13 @@
 <?php
-
+$begin = date('i:s');
 
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
 sfContext::createInstance($configuration)->dispatch();
+
+$end = date('i:s');
+
+if ($_GET['debug']) {
+	echo $begin . '<br/>' . $end;
+}
