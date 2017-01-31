63168a22cc04d6b498a7a38f1dbdb9482efdfd99
githubusukhuu <m.usukhbayar@gmail.com>
2017-01-31 20:13:24 +0800

    cc

--- apps/frontend/modules/orders/templates/productsSuccess.php
+++ apps/frontend/modules/orders/templates/productsSuccess.php
@@ -1,24 +1,26 @@
-<table class="table table-bordered table-striped table-hover">
-    <thead>
-        <tr>
-            <th>Бараа</th>
-            <th>Тоо</th>
-            <th>Үнэ</th>
-            <th>Нийт</th>
-            <th>Нэмэлт</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach ($rows as $row): ?>
+<div style="min-width: 600px;">
+    <table class="table table-bordered table-striped table-hover">
+        <thead>
             <tr>
-                <td><?php echo $row['name'] ?></td>
-                <td><?php echo $row['quantity'] ?></td>
-                <td><?php echo $row['price'] ?></td>
-                <td><?php echo $row['amount'] ?></td>
-                <td>
-                    <?php echo OrderProductDetailsTable::getDetails($row['id']); ?>
-                </td>
+                <th>Бараа</th>
+                <th width="50">Тоо</th>
+                <th width="80">Үнэ</th>
+                <th width="80">Нийт</th>
+                <th width="150">Нэмэлт</th>
             </tr>
-        <?php endforeach; ?>
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            <?php foreach ($rows as $row): ?>
+                <tr>
+                    <td><?php echo $row['name'] ?></td>
+                    <td><?php echo $row['quantity'] ?></td>
+                    <td><?php echo $row['price'] ?></td>
+                    <td><?php echo $row['amount'] ?></td>
+                    <td>
+                        <?php echo OrderProductDetailsTable::getDetails($row['id']); ?>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
