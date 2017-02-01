cf7b76307bd8b95f12ec0046e94b63063f299151
githubusukhuu <m.usukhbayar@gmail.com>
2017-02-01 11:04:02 +0800

    fbPage

--- apps/frontend/modules/contact/templates/indexSuccess.php
+++ apps/frontend/modules/contact/templates/indexSuccess.php
@@ -17,14 +17,14 @@
 <section class="main-container">
     <div class="container">
         <div class="row">
-            <div class="col-md-8">
+            <div class="col-md-7">
                 <!-- google maps start -->
                 <div id="map-canvas"></div>
                 <!-- google maps end -->
             </div>
 
             <!-- sidebar start -->
-            <aside class="col-md-4">
+            <aside class="col-md-5">
                 <div class="sidebar">
                     <div class="side vertical-divider-left">
                         <h3 class="title">Мегамед ХХК</h3>
@@ -35,9 +35,27 @@
                             <li><i class="fa fa-mobile pr-10 pl-5"></i><abbr title="Phone">M:</abbr> (976) 8990-8886</li>
                             <li><i class="fa fa-envelope pr-10"></i><a href="mailto:sales@megamed.mn">sales@megamed.mn</a></li>
                         </ul>
+                        <!--
                         <ul class="social-links colored circle large">
                             <li class="facebook"><a target="_blank" href="https://www.facebook.com/1550776941914587"><i class="fa fa-facebook"></i></a></li>
                         </ul>
+                        -->
+                    </div>
+                    <div class="space"></div>
+                    <div class="side vertical-divider-left">
+                        <div id="fb-root"></div>
+                        <script>
+                            (function (d, s, id) {
+                                var js, fjs = d.getElementsByTagName(s)[0];
+                                if (d.getElementById(id))
+                                    return;
+                                js = d.createElement(s);
+                                js.id = id;
+                                js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=902601439842754";
+                                fjs.parentNode.insertBefore(js, fjs);
+                            }(document, 'script', 'facebook-jssdk'));
+                        </script>
+                        <div class="fb-page" data-href="https://www.facebook.com/megamed.mn/" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/megamed.mn/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/megamed.mn/">Герман шүдний материал</a></blockquote></div>
                     </div>
                 </div>
             </aside>
